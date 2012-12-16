f7abb1a90683690cbe2a7237494bd10e69bc8e64
rganin <rganin@gmail.com>
2012-12-16 20:52:56 +0200

    Path fixes

--- lib/Magelight/Components/Loaders/Modules.php
+++ lib/Magelight/Components/Loaders/Modules.php
@@ -94,7 +94,7 @@ final class Modules
         if (!isset($module['path'])) {
             $module['path'] = $module['name'];
         }
-        $module['path'] = str_replace('\\', DS, $module['path']);
+        $module['path'] = str_replace('\\', '/', $module['path']);
         
         if (\Magelight::app()->isInDeveloperMode() && !$this->moduleExists($module['path'])) {
             throw new \Magelight\Exception('Module "' .  $module['name'] . '" does not exist or not readable.');
