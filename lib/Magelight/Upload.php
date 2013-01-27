844965bc61961b7690f141b1d15d08a6f150f2e7
rganin <rganin@gmail.com>
2013-01-27 13:18:31 +0200

    Minor fixes

--- lib/Magelight/Upload.php
+++ lib/Magelight/Upload.php
@@ -14,46 +14,99 @@ namespace Magelight;
  */
 class Upload
 {
+    /**
+     * Using forgery
+     */
     use \Magelight\Traits\TForgery;
 
+    /**
+     * Uploaded file data
+     *
+     * @var array
+     */
     protected $_fileData = [];
 
+    /**
+     * Forgery constructor
+     *
+     * @param array $data
+     */
     public function __forge($data = [])
     {
         $this->setFileData($data);
     }
 
+    /**
+     * Set uploaded file data
+     *
+     * @param array $data
+     * @return Upload
+     */
     public function setFileData($data = [])
     {
         $this->_fileData = $data;
         return $this;
     }
 
+    /**
+     * Get upload name
+     *
+     * @return string|null
+     */
     public function getName()
     {
         return isset($this->_fileData['name']) ? $this->_fileData['name'] : null;
     }
 
+    /**
+     * Get upload TMP name
+     *
+     * @return string|null
+     */
     public function getTmpName()
     {
         return isset($this->_fileData['tmp_name']) ? $this->_fileData['tmp_name'] : null;
     }
 
+    /**
+     * Get upload size
+     *
+     * @return int|null
+     */
     public function getSize()
     {
         return isset($this->_fileData['size']) ? $this->_fileData['size'] : null;
     }
 
+    /**
+     * Get upload error code
+     *
+     * @return int|null
+     */
     public function getError()
     {
         return isset($this->_fileData['error']) ? $this->_fileData['error'] : null;
     }
 
+    /**
+     * Get upload mime type
+     *
+     * @return string|null
+     */
     public function getType()
     {
         return isset($this->_fileData['type']) ? $this->_fileData['type'] : null;
     }
 
+    /**
+     * Save upload to file
+     *
+     * @param string $path
+     * @param string|null $filename
+     * @param bool $createPath
+     * @param int $mode
+     * @return bool
+     */
     public function saveTo($path, $filename = null, $createPath = true, $mode = 755)
     {
         if (empty($filename)) {
