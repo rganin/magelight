35a9f2bd9fe9d9c9c6850c9b24ab84f94db1ebd7
Roman Ganin <rganin@magento.com>
2016-09-08 15:42:53 +0300

    Updated Ajax form validation to match nearest .control-container element

--- modules/Magelight/Webform/static/js/ajax-form.js
+++ modules/Magelight/Webform/static/js/ajax-form.js
@@ -3,19 +3,26 @@ function bindFormsValidation()
     $('form[data-front-validate]').each(function(){
         var rules = jQuery.parseJSON($(this).attr('data-validator-rules')) || {};
         var messages = jQuery.parseJSON($(this).attr('data-validator-messages')) || {};
+        var controlContainerSelector = $(this).attr('data-control-container-selector') || 'div';
         $(this).validate({
             rules: rules,
             messages: messages,
             errorClass: 'help-inline text-danger',
             ignore: false,
-            highlight: function(label) {
-                $(label).closest('.form-group').removeClass('has-success').addClass('has-error');
+            highlight: function(element) {
+                $(element).closest(controlContainerSelector).removeClass('has-success').addClass('has-error');
             },
-            success: function(label) {
-                label.addClass('valid').closest('.form-group').removeClass('has-error').addClass('has-success');
+            success: function(element) {
+                $(element).addClass('valid').closest(controlContainerSelector).removeClass('has-error').addClass('has-success');
             },
             errorPlacement: function(error, element) {
-                error.appendTo(element.closest('.form-group'));
+                console.log(element.closest(controlContainerSelector).attr('class'));
+                var errorContainer = element.closest(controlContainerSelector).find('.error-container');
+                if (errorContainer.length) {
+                    error.appendTo(errorContainer);
+                } else {
+                    error.appendTo(element.closest('.form-group'));
+                }
             }
         });
     });
